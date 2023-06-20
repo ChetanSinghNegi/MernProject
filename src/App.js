56888c13d5da486341e0617c87f7792558a8c9e9
ChetanSinghNegi <chetannegi000@gmail.com>
2023-06-21 00:06:25 +0530

    added till places

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 const App = () => {
   return (
     <Router>
@@ -16,6 +17,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:uid/places">
+            <UserPlaces />
           </Route>
           <Route path="/places/new">
             <NewPlace />
